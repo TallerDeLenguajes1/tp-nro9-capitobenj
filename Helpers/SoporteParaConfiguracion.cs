c986aa86b7c91f4701bd0712f000f422f3a18108
capitobenj <48660025+capitobenj@users.noreply.github.com>
2019-06-09 01:58:06 -0300

    Add files via upload

---
+++ Helpers/SoporteParaConfiguracion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    class SoporteParaConfiguracion
+    {
+        static public void CrearArchivoDeConfiguracion()
+        {
+            string arch = "config.dat";
+            string[] drives = Directory.GetLogicalDrives();
+            string folder = drives[0] + "nuevo" ;
+            BinaryWriter bin = new BinaryWriter(File.Open(arch, FileMode.Create));
+            bin.Write(folder);
+            bin.Close();
+        }
+        static public string LeerConfiguracion()
+        {
+            string config;
+            BinaryReader bin =new BinaryReader(File.Open("config.dat", FileMode.Open));
+            config = bin.ReadString();
+            return config;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace TP9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string config,texto,morse,date;
+            date = dateTime.ToString("ddMMyyyy");
+
+            if (File.Exists("config.dat"))
+            {
+                SoporteParaConfiguracion.CrearArchivoDeConfiguracion();
+                config = SoporteParaConfiguracion.LeerConfiguracion();
+            }
+            else config = SoporteParaConfiguracion.LeerConfiguracion();
+            moverarchivos(config);
+            Directory.CreateDirectory(config + @"\Morse");
+            Directory.CreateDirectory(config + @"\Texto");
+            Console.WriteLine("Texto a codificar: ");
+            texto =  Console.ReadLine();
+            morse = ConversorDeMorse.TextoAMorse(texto);
+            File.WriteAllText(config +@"\Morse\morse_["+date+"].txt",morse);
+            File.WriteAllText(config+@"\Texto\texto_["+date+"].txt",texto);
+            ConversorDeMorse.MorseAudio(morse,config);
+        }
+        public static void moverarchivos(string config)
+        {
+            Directory.CreateDirectory(config);
+            string dor = Directory.GetCurrentDirectory();
+            string[] files = Directory.GetFiles(dor);
+            string[] items;
+            string exten = ".";
+            int f=files.Length;
+            int ff;
+            for (int cont=0;cont<f ;cont++)
+            {
+                ff = files[cont].Length;
+                exten="."+files[cont].Substring(ff - 3);
+                items = files[cont].Split('\\');
+                int op = items.Length;
+                if (exten == ".mp3" || exten == ".txt")
+                {
+                    File.Copy(files[cont], config + @"\" + items[op-1]);
+                } 
+            }
+            
+        }
+    }
+}
